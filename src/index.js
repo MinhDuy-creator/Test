6f08074e28db90732a999b62825c829d8d3c1a70
MinhDuy-creator <duypro12321@gmail.com>
2021-03-20 22:58:23 +0700

    add database prettier listage and husky

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 const path = require('path');
 const express = require('express');
-const handlebars  = require('express-handlebars');
+const handlebars = require('express-handlebars');
 const app = express();
+const port = 3000;
 const morgan = require('morgan');
 
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //HTTP handle
 app.use(morgan('combined'));
 
 //Teamplate engine
-app.engine('hbs', handlebars({
-  extname:'.hbs'
-}));
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', function (req, res) {
-  res.render('home');
-});
+//Route init
+route(app);
 
-app.get('/news', function (req, res) {
-  res.render('news');
-});
- 
 app.listen(3000);
